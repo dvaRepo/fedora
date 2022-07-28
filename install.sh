35404b5155c14548cf2fe61aba68f1814b3a3b64
dva <60975283+dvaRepo@users.noreply.github.com>
2022-07-28 12:01:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 for i in `cat packages.txt` ; do sudo dnf -y install $i; done
+
+user=$(whoami)
+DIR="/home/$user/suckless"
+if [ ! -d "$DIR" ]; then
+  mkdir "$DIR"
+else
+  echo "${DIR} already exists"
+fi
+
+cd $DIR
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/slock
+
+git clone https://github.com/bakkeby/dwm-flexipatch.git
+git clone https://github.com/yshui/picom.git
